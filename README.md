3834058f17e524b5506a3ed8b0a306112acbb66e
Darlan Araujo <darlanparaujo@outlook.com>
2022-08-01 11:55:04 -0300

    Atualização #2
    A base do projeto foi finalizado. O objetivo agora é gerar mais desafios para o jogo, além de personalizar com mais elementos.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # jogo-mario
  #mini-Projetos - Jogo do Mario feito com HTML, CSS e JS.
+
+============= Briefing =============
+====================================
+
+Criar um jogo baseado no Super Mario.
+* O jogador informara seu nome, que será salvo no LocalStorage.
+* O jogo precisa ter um botão de Play para iniciar a execução.
+* O jogador deve usar as teclas ArrowUp ou Space para fazer o mario pular e assim desviar dos obstaculos ou pegar as moedas.
+* O jogo deve contar o tempo até que o jogador perca a partida.
+* Ao perder a partida deve ser exibido uma tela de game-over.
+* No final do jogo deve ser exibido um ranking com os 10 melhores resultados de tempo e moedas coletadas.
+
+Bonus:
+* Tentar implementar um nivel que faça com que o jogo comece com uma velocidade mais lenta e que vai aumentando.
+
+
+==========================================
+O desenvolvimento desse projeto partiu do vídeo feito no Youtube do canal Manual do Dev. Mas foi aprimorado seguindo meu proprio conhecimento.
+
+Créditos do projeto: https://www.youtube.com/watch?v=r9buAwVBDhA&t=1327s
+
+
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('#mario');
 const pipe = document.querySelector('#pipe');
+const pipe2 = document.querySelector('#pipe2');
 
 const pulo = (event) => {
     if(event.key === ' ' || event.key === 'ArrowUp') {
@@ -17,12 +18,26 @@ document.addEventListener('keydown', pulo);
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom;
+    const pipePosition2 = pipe2.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if()
-
-    if(pipePosition <= 128) {
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition <120 || pipePosition2 <= 120 && pipePosition2 > 0 && marioPosition <120) {
+        // Pipe 1
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        // Pipe 2
+        pipe2.style.animation = 'none';
+        pipe2.style.left = `${pipePosition2}px`;
+
+        // Mario
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = 'images/game-over.png'
+        mario.classList.add('game-over');
+
+        // Encerra o loop;
+        clearInterval(loop);
     }
 }, 10);
